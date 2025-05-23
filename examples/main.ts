8fadfad280f3a35b5f3d8eba8a7ca0fda6862e95
Aman Kumar <aman.kumar74458@gmail.com>
2025-05-24 00:27:29 +0530

    chore: Update example with add annotation functionality with rectangle support

--- examples/main.ts
+++ examples/main.ts
@@ -1,6 +1,8 @@
-import { PdfViewerKit } from '../src/index';
+import { PdfViewerKit, RectangleConfig } from '../src/index';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `<div id="view-pdf"></div>`;
+
+PdfViewerKit.unloadAll();
 
 PdfViewerKit.load({
   document: './pdfs/adient-test.pdf',
@@ -9,4 +11,24 @@ PdfViewerKit.load({
     showThumbnail: true,
     showSearch: true,
   },
-}).then((instances) => {});
+}).then((instances) => {
+  if (instances) {
+    setTimeout(() => {
+      instances.annotation.addAnnotation({
+        pageNumber: 18,
+        x0: 214.5,
+        y0: 272,
+        x1: 185,
+        y1: 47,
+        fillColor: 'transparent',
+        strokeColor: 'red',
+        strokeWidth: 2,
+        strokeStyle: 'Solid',
+        opacity: 1,
+        type: 'rectangle',
+        interactive: false,
+      } as RectangleConfig);
+      instances.goToPage(18);
+    }, 1000);
+  }
+});
